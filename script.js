bdb576c15aed8c8e19fc60d3caef63e9849b5e4f
bthacker <b.thacker@outlook.com>
2026-01-24 19:01:36 -0500

    some upgrades

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
     const typingElement = document.querySelector('.typing-effect');
     const textToType = ""; // Unused
-    const fullText = "./welcome.sh";
+    const command = "cat about.txt";
+    const aboutData = `
+name: Brandon Thacker
+location: Atlanta, Georgia
+role(s): Father. Software Engineer. Game Developer. Tech Enthusiast.
+status: Adapting to the ever-evolving tech landscape in the age of AI.
+hobbies: Cars, Video Games, Poker, Traveling, Collectibles, Horror
+updated: January, 2026
+`;
+    // const fullText = "cat about.txt \n test 123";
     let charIndex = 0;
 
     function typeText() {
-        if (charIndex < fullText.length) {
-            typingElement.textContent += fullText.charAt(charIndex);
+        if (charIndex < command.length) {
+            typingElement.textContent += command.charAt(charIndex);
             charIndex++;
             setTimeout(typeText, 50 + Math.random() * 100); // Random typing speed
+        } else if (charIndex === command.length) {
+            // Emulate file reading delay or just output
+            setTimeout(() => {
+                typingElement.textContent += "\n" + aboutData;
+            }, 300);
+            charIndex++;
         }
     }
 
